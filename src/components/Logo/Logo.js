1f11ec261607ef710a8948347ac929c73f7d5552
Hunter <hmvitous@yahoo.com>
2020-05-28 15:55:38 +0200

    Adds logo image to the site

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Tilt from "react-tilt";
 import './Logo.css'
+import search from './search.png'
 
 const Logo = () => {
   return (
     <div className="ma4 ,mt0">
       <Tilt
         className="Tilt br2 shadow-2"
-        options={{ max: 25 }}
+        options={{ max: 60 }}
         style={{ height: 250, width: 250 }}
       >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner"><img alt='Face Search Logo' src={search}/></div>
       </Tilt>
     </div>
   );
