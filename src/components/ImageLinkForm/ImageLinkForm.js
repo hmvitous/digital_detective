ab57cd154e9ff06ce1d0ce35912f924761c3709b
Hunter <hmvitous@yahoo.com>
2020-05-28 18:03:10 +0200

    Adds a background pattern to the input area

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,16 @@
 import React from "react";
+import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
   return (
     <div>
       <p className="f3">
         {
-          "This Digital Detective will find the faces in your pictures. Put him on the case"
+          "This Digital Detective will find the faces in your pictures. Put him on the case!"
         }
       </p>
       <div className="center">
-        <div className="center pa4 br3 shadow-5">
+        <div className="form center pa4 br3 shadow-5">
           <input className="f4 pa2 w-70 center" type="text" />
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
             Detect
