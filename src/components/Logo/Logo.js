2afdd19d8ca28f3c967c060f714f246ed957827c
Hunter <hmvitous@yahoo.com>
2020-05-28 15:48:09 +0200

    Adds a css file for the logo which only has a background styling

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Tilt from "react-tilt";
+import './Logo.css'
 
 const Logo = () => {
   return (
     <div className="ma4 ,mt0">
       <Tilt
-        className="Tilt"
+        className="Tilt br2 shadow-2"
         options={{ max: 25 }}
         style={{ height: 250, width: 250 }}
       >
