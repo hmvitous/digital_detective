0f057f0930097da111ffa95423cd49c3c77e5e37
Hunter <hmvitous@yahoo.com>
2020-05-28 15:25:23 +0200

    Installs tachyons and styles the sign out to the right side

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Navigation from './components/Navigation'
+import Navigation from './components/Navigation/Navigation'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Navigation />
-    <Logo />
+    {/* <Logo />
     <ImageLinkForm />
-    <FaceRecognition />
+    <FaceRecognition /> */}
     </div>
   );
 }
