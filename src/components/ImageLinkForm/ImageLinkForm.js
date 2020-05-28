e252fed80e07626c16eccadcb554cba0c1a8b27b
Hunter <hmvitous@yahoo.com>
2020-05-28 17:49:43 +0200

    Sets up the image link form component just like the previous components

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ImageLinkForm = () => {
+  return;
+  <div>
+    <p className="f3">
+      {
+        "This Digital Detective will find the faces in your pictures. Put him on the case"
+      }
+    </p>
+  </div>;
+};
+
+export default ImageLinkForm;
