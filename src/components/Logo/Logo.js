f73e53fc1fabd04029d54e6bead171d3f065d7fc
Hunter <hmvitous@yahoo.com>
2020-05-28 15:43:37 +0200

    Imports tilt adds places a placeholder image for the logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
 
 const Logo = () => {
   return (
-    <div className='ma4 ,mt0'>
-
+    <div className="ma4 ,mt0">
+      <Tilt
+        className="Tilt"
+        options={{ max: 25 }}
+        style={{ height: 250, width: 250 }}
+      >
+        <div className="Tilt-inner"> 👽 </div>
+      </Tilt>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default Logo
+export default Logo;
