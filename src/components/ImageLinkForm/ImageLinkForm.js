458c75a98c8fd4ebeb75a58b8aa37b5c45b7c24e
Hunter <hmvitous@yahoo.com>
2020-05-28 17:53:24 +0200

    Creates an input and a button with tachyon styling

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 const ImageLinkForm = () => {
-  return;
-  <div>
-    <p className="f3">
-      {
-        "This Digital Detective will find the faces in your pictures. Put him on the case"
-      }
-    </p>
-  </div>;
+  return (
+    <div>
+      <p className="f3">
+        {
+          "This Digital Detective will find the faces in your pictures. Put him on the case"
+        }
+      </p>
+      <div>
+        <input className="f4 pa2 w-70 center" type="text" />
+        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          Detect
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ImageLinkForm;
