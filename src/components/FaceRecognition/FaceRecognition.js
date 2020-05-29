52610829fcf508d2944d1ae2de4b3b3c9e34b324
Hunter <hmvitous@yahoo.com>
2020-05-29 15:49:20 +0200

    Adds dimensioning to the images that are entered in the input

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className='center'>
-      <img alt="" src={imageUrl} />
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img alt="" src={imageUrl} width="500px" height="auto" />
+      </div>
     </div>
   );
 };
 
-
-export default FaceRecognition
+export default FaceRecognition;
