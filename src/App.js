5006100f5c04f2f697b3ed9db2013e010a2e116f
Hunter <hmvitous@yahoo.com>
2020-05-29 17:06:11 +0200

    Sets the box state to empty object and creates the calculatefacelocation function

--- src/App.js
+++ src/App.js
@@ -30,28 +30,24 @@ class App extends Component {
     this.state = {
       input: "",
       imageUrl: "",
+      box: {},
     };
   }
 
+  calculateFaceLocation = (data) => {
+    response.outputs[0].data.regions[0].region_info.bounding_box
+  };
+
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   };
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input
-      )
-      .then(
-        function (response) {
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        },
-        function (err) {
-          // there was an error
-        }
-      );
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => this.calculateFaceLocation(response))
+      .catch((error) => console.log(error))
   };
 
   render() {
