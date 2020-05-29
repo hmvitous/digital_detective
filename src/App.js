c9763dd1b05a34a440d1bc6641f6654f91e24c8d
Hunter <hmvitous@yahoo.com>
2020-05-29 15:46:31 +0200

    Gets the clarafai api to find a face Only console logging right now and nothing shows on the image

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
@@ -29,23 +29,24 @@ class App extends Component {
     super();
     this.state = {
       input: "",
+      imageUrl: "",
     };
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   };
 
   onSubmit = () => {
-    console.log("click");
+    this.setState({ imageUrl: this.state.input });
     app.models
       .predict(
-        "a403429f2ddf4b49b307e318f00e528b",
-        "https://samples.clarifai.com/face-det.jpg"
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input
       )
       .then(
         function (response) {
-          console.log(response);
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function (err) {
           // there was an error
@@ -64,7 +65,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onSubmit={this.onSubmit}
         />
-        <FaceRecognition /> 
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
