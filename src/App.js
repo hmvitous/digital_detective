12aade3a01f18c54d3204648639f13d46285939f
Hunter <hmvitous@yahoo.com>
2020-06-10 15:24:46 +0200

    Sets up a loadUser function for a user profile

--- src/App.js
+++ src/App.js
@@ -35,8 +35,29 @@ class App extends Component {
       box: {},
       route: "signin",
       isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      },
     };
   }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        user: {
+          id: data.id,
+          name: data.name,
+          email: data.email,
+          entries: data.entries,
+          joined: data.joined,
+        },
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
     const clarafaiFace =
