0bc74727eac37139eb4258241c19dc835ae9e2e2
Hunter <hmvitous@yahoo.com>
2020-06-16 15:03:31 +0200

    Removes api call to be placed into the back end code

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import Rank from "./components/Rank/Rank";
 import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
 import "./App.css";
-
-const app = new Clarifai.App({
-  apiKey: "daac70cd5aa14b5fa261d512d1db8212",
-});
 
 const particlesOptions = {
   particles: {
@@ -83,8 +78,14 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then(response => response.json())
       .then((response) => {
         if (response) {
           fetch("http://localhost:3000/image", {
@@ -98,7 +99,7 @@ class App extends Component {
             .then((count) => {
               this.setState(Object.assign(this.state.user, { entries: count }));
             })
-            .catch(console.log)
+            .catch(console.log);
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
