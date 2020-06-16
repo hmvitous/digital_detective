98edda9602e54920172f0708c7535d699f06743e
Hunter <hmvitous@yahoo.com>
2020-06-16 16:00:22 +0200

    Fixes a typo

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("https://peaceful-lake-33404.herokuapp.com//imageurl", {
+    fetch("https://peaceful-lake-33404.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -88,7 +88,7 @@ class App extends Component {
       .then(response => response.json())
       .then((response) => {
         if (response) {
-          fetch("https://peaceful-lake-33404.herokuapp.com//image", {
+          fetch("https://peaceful-lake-33404.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("https://peaceful-lake-33404.herokuapp.com//register", {
+    fetch("https://peaceful-lake-33404.herokuapp.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
